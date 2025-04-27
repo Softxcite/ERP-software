3495ca0aa8eae081e90b29c018b6d69816cc5f1a
Dev-aniket001 <softxcite@gmail.com>
2025-04-27 18:31:13 +0530

    login theam change template

--- erp-software/resources/views/auth/login.blade.php
+++ erp-software/resources/views/auth/login.blade.php
@@ -1,4 +1,133 @@
-<x-guest-layout>
+<x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-startbar="dark" data-bs-theme="light">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Login | Materialy - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
+
+
+    <!-- App css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }} " rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+
+<!-- Top Bar Start -->
+
+<body>
+    <div class="container-xxl">
+        <div class="row vh-100 d-flex justify-content-center">
+            <div class="col-12 align-self-center">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-4 mx-auto">
+                            <div class="card">
+                                <div class="card-body p-0 bg-black auth-header-box rounded-top">
+                                    <div class="text-center p-3">
+                                        <a href="./" class="logo logo-admin">
+                                            <img src="{{ asset('assets/images/logo-sm.png') }}" height="50"
+                                                alt="logo" class="auth-logo">
+                                        </a>
+                                        <h4 class="mt-3 mb-1 fw-semibold text-white fs-18">Let's Get Started Materialy
+                                        </h4>
+                                        <p class="text-muted fw-medium mb-0">Sign in to continue to Materialy.</p>
+                                    </div>
+                                </div>
+                                <div class="card-body pt-0">
+                                    <form class="my-4" method="POST" action="{{ route('login') }}">
+                                        <div class="form-group mb-2">
+                                            <label class="form-label" for="username">Username</label>
+                                            <x-input-label for="email" :value="__('Email')" />
+                                            <input class="form-control" type="email" name="email" id="email"
+                                                :value="old('email')" required autofocus autocomplete="username"
+                                                placeholder="Enter username">
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div><!--end form-group-->
+
+                                        <div class="form-group">
+                                            <label class="form-label" for="userpassword">Password</label>
+                                            <x-input-label for="password" :value="__('Password')" />
+                                            <input class="form-control" id="password" class="block mt-1 w-full"
+                                                type="password" name="password" required autocomplete="current-password"
+                                                placeholder="Enter password">
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div><!--end form-group-->
+
+                                        <div class="form-group row mt-3">
+                                            <div class="col-sm-6">
+                                                <div class="form-check form-switch form-switch-primary">
+                                                    {{-- <input class="form-check-input" type="checkbox"
+                                                        id="customSwitchprimary">
+                                                    <label class="form-check-label" for="customSwitchprimary">Remember
+                                                        me</label> --}}
+                                                    <div class="block mt-4">
+                                                        <label for="remember_me" class="inline-flex items-center">
+                                                            <input id="remember_me" type="checkbox"
+                                                                class="rounded dark:bg-gray-900 form-check-input border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
+                                                                name="remember">
+                                                            <span
+                                                                class="ms-2 text-sm text-gray-600 form-check-label dark:text-gray-400">{{ __('Remember me') }}</span>
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div><!--end col-->
+                                            <div class="col-sm-6 text-end">
+                                                <a href="auth-recover-pw.html" class="text-muted font-13"><i
+                                                        class="dripicons-lock"></i> Forgot password?</a>
+                                            </div><!--end col-->
+                                        </div><!--end form-group-->
+
+                                        <div class="form-group mb-0 row">
+                                            <div class="col-12">
+                                                <div class="d-grid mt-3">
+                                                    <button class="btn btn-primary" type="button">Log In <i
+                                                            class="fas fa-sign-in-alt ms-1"></i></button>
+                                                </div>
+                                            </div><!--end col-->
+                                        </div> <!--end form-group-->
+                                    </form><!--end form-->
+                                    <div class="text-center  mb-2">
+                                        <p class="text-muted">Don't have an account ? <a href="auth-register.html"
+                                                class="text-primary ms-2">Free Resister</a></p>
+                                        <h6 class="px-3 d-inline-block">Or Login With</h6>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+                                        <a href="#"
+                                            class="d-flex justify-content-center align-items-center thumb-md bg-blue-subtle text-blue rounded-circle me-2">
+                                            <i class="fab fa-facebook align-self-center"></i>
+                                        </a>
+                                        <a href="#"
+                                            class="d-flex justify-content-center align-items-center thumb-md bg-info-subtle text-info rounded-circle me-2">
+                                            <i class="fab fa-twitter align-self-center"></i>
+                                        </a>
+                                        <a href="#"
+                                            class="d-flex justify-content-center align-items-center thumb-md bg-danger-subtle text-danger rounded-circle">
+                                            <i class="fab fa-google align-self-center"></i>
+                                        </a>
+                                    </div>
+                                </div><!--end card-body-->
+                            </div><!--end card-->
+                        </div><!--end col-->
+                    </div><!--end row-->
+                </div><!--end card-body-->
+            </div><!--end col-->
+        </div><!--end row-->
+    </div><!-- container -->
+</body>
+<!--end body-->
+
+</html>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +173,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
